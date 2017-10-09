bcf5eecb3e5193c9dd5951c9ce28654ccb92a41a
yxfff <514637059@qq.com>
2017-10-09 17:06:57 +0100

    LeetCode 160 Intersection of Two Linked Lists

---
+++ LinkedList/CrossoverList.java
@@ -0,0 +1,94 @@
+package com.classes.one;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class CrossoverList {
+	public static void main(String[] args) {
+		ListNode A = new ListNode(1);
+		ListNode A1 = new ListNode(2);
+		ListNode A2 = new ListNode(3);
+		ListNode A3 = new ListNode(4);
+		
+		A.next = A1;
+		A1.next = A2;
+		A2.next = A3;
+	
+		ListNode B = new ListNode(5);
+		ListNode B1 = new ListNode(2);
+		ListNode B2 = new ListNode(3);
+		ListNode B3 = new ListNode(1);
+		
+		B.next = B1;
+		B1.next = B2;
+		B2.next = B3;
+		
+		ListNode t = new SolutionThree().getIntersectionNode(A, B);
+		System.out.println(t.val);
+	}
+}
+
+class SolutionThree{
+	//方法1：超时,暴力求解，时间复杂度高
+	public ListNode getIntersectionNode(ListNode headA,ListNode headB){
+		Set<ListNode> temp = new HashSet<ListNode>();
+		while(headA!=null){
+			temp.add(headA);
+			headA = headA.next;
+		}
+		while(headB!=null){
+			Iterator<ListNode> it = temp.iterator();
+			while(it.hasNext()){
+				if(it.next().val == headB.val){
+					return headB;
+				}
+			}
+			headB = headB.next;
+		}
+		return null;
+	}
+	
+	//方法2：
+	public int get_list_length(ListNode head){//计算链表长度
+		int len = 0;
+		while(head != null){
+			len++;
+			head = head.next;
+		}
+		return len;
+	}
+	
+	public ListNode forward_long_list(int long_len, int short_len,ListNode head){//链表向前移动至多出节点个数后面的位置
+		int delta = long_len - short_len;
+		while(head!=null && delta>0){
+			head = head.next;
+			delta--;
+		}
+		return head;
+	}
+	
+	public ListNode getIntersectionNode_two(ListNode headA,ListNode headB){
+		int len_headA = get_list_length(headA);
+		int len_headB = get_list_length(headB);
+		
+		if(len_headA > len_headB){
+			headA = forward_long_list(len_headA,len_headB,headA);
+		}else{
+			headB = forward_long_list(len_headB,len_headA,headB);
+		}
+		
+		while(headA!=null){
+			if(headA.val == headB.val){
+				return headA;
+			}else{
+				headA = headA.next;
+				headB = headB.next;
+			}
+		}
+		return null;
+	}
+	
+	
+	
+}
--- LinkedList/README.txt
+++ LinkedList/README.txt
@@ -7,11 +7,12 @@ https://leetcode.com/problems/reverse-linked-list/description/
 思路：
 	依次遍历链表节点，每遍历一个节点即逆置一个节点
 	代码为ReserveLinkedList_one.java
-
+---------------------------------------------------------------------
 2.链表逆序
 LeetCode 92. Reverse LinkedList
 
 已知链表头节点指针head，将链表从位置m到n逆序（不申请额外空间）
+
 https://leetcode.com/problems/reverse-linked-list-ii/description/
 
 思路:
@@ -20,3 +21,26 @@ https://leetcode.com/problems/reverse-linked-list-ii/description/
 3.从head开始逆置change_len = n-m+1个节点
 4.将pre_head和new_head连接，modify_list_tail与head连接
 代码为ReserveLinkedList_two.java
+----------------------------------------------------------------------
+3.求两个链表的交点
+LeetCode 160
+已知链表A的头节点指针headA，链表B的头节点指针headB，两个链表相交，
+求两个链表交点对应的节点
+
+1.如果两个链表没有交点，则返回NULL
+2.在求交点过程中，不可以破坏链表的结构或者修改链表的数据域
+3.可以确保传入的链表A与链表B没有任何环
+4.时间复杂度O(n),空间复杂度O(1)
+https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+
+
+方法1：
+思路：
+ 使用set集合
+ 遍历链表A，将A中节点对应的地址插入到set中
+ 遍历链表B，将B中节点对应的地址，在set中查找，发现在set中的第一个节点位置即
+ 两个链表的交点。   
+方法2：
+ 计算headA的长度和headB的长度，较长的链表多出的长度
+ 将较长的链表指针移动到和较短的链表指针对齐的位置
+ headA和headB同时移动，当两指针指向同一节点时即找到了。
